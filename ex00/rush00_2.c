5483c8dea30956a44e079eb42674745805ba8a3d
Munenori Iida <miida@c1r18s8.42tokyo.jp>
2022-10-08 01:06:15 +0900

    ex00/rush00_2.cに出力文字を変数化したバージョンを置きました。原則ex00/rush00_2.cを使ってください。

--- ex00/rush00_2.c
+++ ex00/rush00_2.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -19,7 +17,15 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 	return ;
 }
-*/
+
+#define T_L 'o'
+#define T_R 'o'
+#define B_L 'o'
+#define B_R 'o'
+#define HORIZ '-'
+#define VERT '|'
+#define SP ' '
+#define EOL '\n'
 
 // x、yと呼ばれる、2つのint型の値を引数とする関数rushを作成せよ。
 //横の長さがx、高さがyの長方形を標準出力に出力すること。
@@ -38,37 +44,21 @@ int	rush(int x, int y)
 		while (xx <= x)
 		{
 			if (xx == 0 && yy == 0)
-			{
-				ft_putchar('o');
-			}
+				ft_putchar(T_L);
 			else if (xx == x - 1 && yy == 0)
-			{
-				ft_putchar('o');
-			}
+				ft_putchar(T_R);
 			else if (xx == 0 && yy == y - 1)
-			{
-				ft_putchar('o');
-			}
+				ft_putchar(B_L);
 			else if (xx == x - 1 && yy == y - 1)
-			{
-				ft_putchar('o');
-			}
+				ft_putchar(B_R);
 			else if (xx == 0 || xx == x - 1)
-			{
-				ft_putchar('|');
-			}
+				ft_putchar(VERT);
 			else if (xx == x)
-			{
-				ft_putchar('\n');
-			}
+				ft_putchar(EOL);
 			else if (yy == 0 || yy == y - 1)
-			{
-				ft_putchar('-');
-			}
+				ft_putchar(HORIZ);
 			else
-			{
-				ft_putchar(' ');
-			}
+				ft_putchar(SP);
 			xx += 1;
 		}
 		yy += 1;
@@ -76,10 +66,10 @@ int	rush(int x, int y)
 	return (0);
 }
 
-/*
 int	main(void)
 {
 	rush(5, 5);
+	rush(5, 1);
+	rush(1, 5);
 	return (0);
 }
-*/
