9146b912e874ecb23a546e2a1ee4f9ec39c5eb5b
Colorlessx7 <yuppiy2000@icloud.com>
2022-10-08 21:02:19 +0900

    atoi_mod

--- ex00/main_atoi.c
+++ ex00/main_atoi.c
@@ -1,23 +1,57 @@
-include<unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_atoi.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusyamas <yuppiy2000@icloud.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 20:25:20 by yusyamas          #+#    #+#             */
+/*   Updated: 2022/10/08 20:59:04 by yusyamas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	void string_to_int(char c[])
+#include <unistd.h>
+#define FT_NULL '\0'
+
+int	c_rush_00_atoi(char *c, int *flag)
 {
-	int	int_num[2];
+	int	num;
 	int	cnt;
 
+	num = 0;
 	cnt = 0;
-	while (c[cnt])
+	while (*c != FT_NULL)
 	{
-		int_num[cnt] = (int)c[cnt];
-		cnt++;
+		num *= 10;
+		num += *c - 48;
+		c += 1;
 	}
-	write(1, &int_num, 1);
-	write(1, &int_num[1], 1);
+	if (num < 0)
+	{
+		*flag = 0;
+	}
+	return (num);
 }
 
 int	main(int argc, char *argv[])
 {
-	if (0 < argc)
-		string_to_int(argv[1]);
+	int	x;
+	int	y;
+	int	flag;
+
+	flag = 1;
+	x = 1;
+	y = 1;
+	if (argc >= 3)
+	{
+		x = c_rush_00_atoi(argv[1], &flag);
+		y = c_rush_00_atoi(argv[2], &flag);
+	}
+	if (flag == 0)
+	{
+		x = 1;
+		y = 1;
+	}
+	rush(x, y);
 	return (0);
 }
